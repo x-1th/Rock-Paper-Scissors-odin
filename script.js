99c733189770f70c0f4e14c0c9c87f5c180fad2d
x-1th <295836686+x-1th@users.noreply.github.com>
2026-09-05 15:06:17 +0300

    Add function to generate computer choice

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+function getComputerChoice() {
+    let computerRandom = Math.floor(Math.random() * 3) + 1;
+    let randomStr = "";
+
+    console.log("Computer random number:", computerRandom);
+    console.log("Before switch:", randomStr);
+
+    switch (computerRandom) {
+        case 1:
+            randomStr = "rock";
+            console.log("Computer choice:", randomStr);
+            break;
+        case 2:
+            randomStr = "paper"
+            console.log("Computer choice:", randomStr);
+            break;
+        case 3:
+            randomStr = "scissors"
+            console.log("Computer choice:", randomStr);
+            break;
+        default:
+            console.log("wrong choice");
+    }
+
+
+    return randomStr;
+
+}
+
+getComputerChoice();
